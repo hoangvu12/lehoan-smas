c1e0b1451b102979f0a22ddf9b23acb74fdd3a75
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-03-26 22:22:08 +0700

    Add redis cache for faster loading experience.

--- src/routers/api.js
+++ src/routers/api.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
+const cache = require("express-redis-cache")({
+  host: "141.136.47.42",
+  port: 6379,
+  expire: 3600, // 1 Hour
+});
+
 const controller = require("../controllers/api.controller");
 
 router.get(
   "/search/class/:className/student/:studentName",
+  cache.route(),
   controller.searchStudent
 );
 
 router.get(
   "/search/class/:className/student/:studentName/study",
+  cache.route(),
   controller.searchStudent,
   controller.getStudy
 );
 
-router.get("/class/:classInput", controller.getClassContact);
-router.get("/class/:classID/student/:studentID", controller.getStudentInfo);
+router.get("/class/:classInput", cache.route(), controller.getClassContact);
+router.get(
+  "/class/:classID/student/:studentID",
+  cache.route(),
+  controller.getStudentInfo
+);
 router.get(
   "/class/:classID/student/:studentID/study",
+  cache.route(),
   controller.getStudentInfo,
   controller.getStudy
 );
