e2e2299906e394265c4a032852565339bd45d3ab
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-03-26 22:12:32 +0700

    Add Open Graph Tags for SEO

--- src/controllers/site.controller.js
+++ src/controllers/site.controller.js
@@ -1,10 +1,22 @@
+const API = require('../utils/API');
+
 class SiteController {
   static search(req, res) {
     res.render("pages/index.ejs");
   }
 
-  static studentInfo(req, res) {
-    res.render("pages/student.ejs", { student: req.params });
+  static async studentInfo(req, res) {
+    const { classID, studentID } = req.params;
+    
+    const { data: contact } = await API.getContact(classID);
+
+    const student = contact.find(
+      (student) => student.pupil_id === Number(studentID)
+    );
+    
+    student.class_id = classID;
+    
+    res.render("pages/student.ejs", { student });
   }
 }
 
